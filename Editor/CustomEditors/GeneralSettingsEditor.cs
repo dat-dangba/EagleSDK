f0860b372da29eda4dd8961caf2b034934abb736
dat-dangba <dat.dangba@tapbi.vn>
2026-07-30 10:40:44 +0700

    Update GeneralSettingsEditor.cs

--- Editor/CustomEditors/GeneralSettingsEditor.cs
+++ Editor/CustomEditors/GeneralSettingsEditor.cs
@@ -25,46 +25,46 @@ namespace Eagle
 
             root.Add(createProjectStructure);
 
-            Button installAllPackage = new Button(InstallAllPackage)
-            {
-                text = "Install All Package",
-            };
-
-            root.Add(installAllPackage);
+            // Button installAllPackage = new Button(InstallAllPackage)
+            // {
+            //     text = "Install All Package",
+            // };
+            //
+            // root.Add(installAllPackage);
 
             HideScript(root);
 
             return root;
         }
 
-        private void InstallAllPackage()
-        {
-            string token = EagleServices.GetToken();
-            if (string.IsNullOrEmpty(token))
-            {
-                EagleLog.Log($"Nhập token trước khi cài");
-                return;
-            }
-
-            RegistryHelper.AddRegistryMAX();
-
-            var package = new List<string>
-            {
-                "com.unity.purchasing",
-                "com.applovin.mediation.ads",
-                AdjustSettingEditor.LinkInstall,
-                $"https://{token}@github.com/dat-dangba/EagleAnalytics.git",
-                $"https://{token}@github.com/dat-dangba/EagleAds.git",
-                $"https://{token}@github.com/dat-dangba/EagleIAP.git",
-                $"https://{token}@github.com/dat-dangba/EagleFirebaseApp.git",
-                $"https://{token}@github.com/dat-dangba/EagleFirebaseAnalytics.git",
-                $"https://{token}@github.com/dat-dangba/EagleFirebaseCrashlytics.git",
-                $"https://{token}@github.com/dat-dangba/EagleFirebaseRemoteConfig.git",
-                $"https://{token}@github.com/dat-dangba/EagleFirebase.git",
-                $"https://{token}@github.com/dat-dangba/EagleBaseGame.git",
-            };
-            InstallPackageHelper.Install(package,
-                () => { CreateAssets.CreateAsset<MAXSetting>(Constant.SettingsFolder); });
-        }
+        // private void InstallAllPackage()
+        // {
+        //     string token = EagleServices.GetToken();
+        //     if (string.IsNullOrEmpty(token))
+        //     {
+        //         EagleLog.Log($"Nhập token trước khi cài");
+        //         return;
+        //     }
+        //
+        //     RegistryHelper.AddRegistryMAX();
+        //
+        //     var package = new List<string>
+        //     {
+        //         "com.unity.purchasing",
+        //         "com.applovin.mediation.ads",
+        //         AdjustSettingEditor.LinkInstall,
+        //         $"https://{token}@github.com/dat-dangba/EagleAnalytics.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleAds.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleIAP.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleFirebaseApp.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleFirebaseAnalytics.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleFirebaseCrashlytics.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleFirebaseRemoteConfig.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleFirebase.git",
+        //         $"https://{token}@github.com/dat-dangba/EagleBaseGame.git",
+        //     };
+        //     InstallPackageHelper.Install(package,
+        //         () => { CreateAssets.CreateAsset<MAXSetting>(Constant.SettingsFolder); });
+        // }
     }
 }
